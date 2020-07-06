8ea79a7e8861b08d7f355bdd3a0b94da47810b14
HyeRim Park <gpflawkd2@gmail.com>
2020-07-06 23:35:42 +0900

    Add Model Folder & Update Model Binding

--- MyApp/Controllers/HomeController.cs
+++ MyApp/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MyApp.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,12 +11,21 @@ namespace MyApp.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: /<controller>/
+        //GET: /<controller>/
         //IActionResult: 함수안에 짜놓은 코드들을 알맞게 View에 매핑해서 return함
         //Views 폴더에 Controller Class의 이름과 동일한 폴더명이 있어야함
         //Views > Home 폴더에 Controller Class의 Action 함수명(Student)과 동일한 Razor View 파일을 추가해주어야함
-        //Razor View 파일: C#코드가 HTML코드와 함께 렌더링되는 View 파일
+        //Razor View를 display 하는 역할
         public IActionResult Student()
+        {
+            return View();
+        }
+
+        //HTML View에서 넘어오는 값들을 받는 역할
+        [HttpPost]
+        //받고 싶은 데이터만 받는 방법: Bind 사용
+        //public IActionResult Student([Bind("Name, Age")] Student model)
+        public IActionResult Student(Student model)
         {
             return View();
         }
---
+++ MyApp/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyApp.Models
+{
+    public class Student
+    {
+        //특정 필드만 제외시킬 때 BindNever 사용
+        [BindNever]
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+        public string Country { get; set; }
+    }
+}
