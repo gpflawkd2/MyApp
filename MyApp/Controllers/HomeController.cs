50d1c43c25605b21d537cb6e75709c5277e4e104
HyeRim Park <gpflawkd2@gmail.com>
2020-07-15 00:22:07 +0900

    Add ViewModel Code

--- MyApp/Controllers/HomeController.cs
+++ MyApp/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using MyApp.Models;
+using MyApp.ViewModels;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -18,7 +19,23 @@ namespace MyApp.Controllers
         //Razor View를 display 하는 역할
         public IActionResult Student()
         {
-            return View();
+            List<Teacher> teachers = new List<Teacher>()
+            {
+                new Teacher(){Name="세종대왕", Class="한글"},
+                new Teacher(){Name="이순신", Class="해상전략"},
+                new Teacher(){Name="제갈량", Class ="지략"},
+                new Teacher(){Name="을지문덕", Class = "지상전략"}
+            };
+
+            //StudentTeacherViewModel 인스턴스화
+            var viewModel = new StudentTeacherViewModel()
+            {
+                Student = new Student(),
+                Teachers = teachers
+            };
+
+            //return View();
+            return View(viewModel);
         }
 
         //HTML View에서 넘어오는 값들을 받는 역할
@@ -26,7 +43,7 @@ namespace MyApp.Controllers
         [HttpPost]
         // 사이트간 요청 위조 예방 > html페이지 Form의 Token 체크(recommend!!)
         [ValidateAntiForgeryToken]
-        public IActionResult Student(Student model)
+        public IActionResult Student(StudentTeacherViewModel model)
         //받고 싶은 데이터만 받는 방법: Bind 사용
         //public IActionResult Student([Bind("Name, Age")] Student model)
         {
