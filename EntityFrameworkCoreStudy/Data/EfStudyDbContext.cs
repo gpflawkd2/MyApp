871ba7dbe68bf85e1dfe8124db545d60b0f67237
gpflawkd2 <gpflawkd2@gmail.com>
2021-04-12 13:19:12 +0900

    Add Model-Class Mapping

--- EntityFrameworkCoreStudy/Data/EfStudyDbContext.cs
+++ EntityFrameworkCoreStudy/Data/EfStudyDbContext.cs
@@ -12,5 +12,15 @@ namespace EntityFrameworkCoreStudy.Data
         {
             optionsBuilder.UseSqlServer(@"Server=localhost;Database=AspnetNoteDb;User Id=Board;Password=phr8611!;");
         }
+
+        
+        // Model-Class Mapping
+        // 2) DBContext -> OnModelCreating() -> EF Fluent API
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // TODO Code
+            //modelBuilder.Entity<User>().ToTable("s_users");
+            //modelBuilder.Entity<User>().Property(p => p.UserName).HasColumnName("s_userName");
+        }
     }
 }
--- EntityFrameworkCoreStudy/Models/User.cs
+++ EntityFrameworkCoreStudy/Models/User.cs
@@ -1,11 +1,19 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EntityFrameworkCoreStudy.Models
 {
+    // Model Class Mapping
+    // 1) Annotaion Mapping
+
+    // Mapping 해야할 테이블명
+    // [Table("s_users")]
     public class User
     {
         [Key]
         public int UserNo { get; set; }
+        // Mapping 해야할 컬럼명
+        // [Column("s_userName")]
         public string UserName { get; set; }
         public string UserId { get; set; }
         public string UserPassword { get; set; }
